72bf72e1149cf56961d601f0c634679a5969fe4a
NodesXV <EShaoBruh@Erics-MBP.fios-router.home>
2022-07-14 20:03:16 -0400

    3rd final

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles
 import {Typography} from '@material-ui/core'; 
 import NavBar from './components/NavBar'
 import Grid from './components/grid'
+import Footer from './components/Footer'
 
 import './App.css';
 
@@ -64,10 +65,22 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <NavBar/>
-        <Grid icon={<SecurityIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>}  title="Secure" btnTitle="Show me More" />
+        
+        <div className={`${classes.grid} ${classes.bigSpace}`}> 
+        <Grid icon={<SecurityIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>} title="Secure" btnTitle="Show me More" />
         <Grid icon={<EventNoteIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Reliable" btnTitle="Show me More"/>
-        <Grid icon={<TrendingUpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>}  title="Performant" btnTitle="Show me More"/>
-      </ThemeProvider>
+        <Grid icon={<TrendingUpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>} title="Performant" btnTitle="Show me More"/>
+        </div>
+
+        <div className={`${classes.grid} ${classes.bigSpace}`}>
+        <Grid icon={<SecurityIcon style={{fill: "#5EA780", height: "125", width: "125"}}/>} title="Secure" btnTitle="Show me More"/>
+        <Grid icon={<EventNoteIcon style={{fill: "E69426", height: "125", width: "125"}}/>} title="Reliable" btnTitle="Show me More"/>
+        <Grid icon={<TrendingUpIcon style={{fill: "#2EA09D", height: "125", width: "125"}}/>} title="Performant" btnTitle="Show me More"/>
+        </div>
+        <div className={classes.bigSpace}>
+          <Footer/>
+        </div>
+        </ThemeProvider>
     </div>
   );
 }
